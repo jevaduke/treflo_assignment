c7d946c39c9eac09d51f89d3b738bf20f7e9fbb1
Jeva <jevaduke@gmail.com>
2022-03-29 00:45:41 +0530

    day-2

---
+++ src/Resourses/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../NavBar/NavBar";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <NavBar />
+      
+    </div>
+  );
+};
+
+export default Home;
